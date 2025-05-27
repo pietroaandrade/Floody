4fce2f3ba1d825bbe546a9f76fa342bfe8bb4e1e
pietroaa <167457324+pietroaandrade@users.noreply.github.com>
2025-05-27 20:59:36 -0300

    feat.map&markers

---
+++ frontend/floody-frontend/src/main.jsx
@@ -0,0 +1,12 @@
+
+import { createRoot } from 'react-dom/client'
+
+import App from "./pages/App"
+
+const root = createRoot(document.getElementById('root'))
+
+root.render(
+  <>
+    <App />
+  </>
+)
