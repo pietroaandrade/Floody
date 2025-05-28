8c508b30194322f822d5c2ea13c6b31213de2c09
pietroaa <167457324+pietroaandrade@users.noreply.github.com>
2025-05-28 17:44:53 -0300

    feat.plan_component

--- frontend/floody-frontend/src/components/Navbar/Navbar.jsx
+++ frontend/floody-frontend/src/components/Navbar/Navbar.jsx
@@ -2,17 +2,19 @@
 import AccountToggle from "./AccountToggle"
 import SearchBar from "./SearchBar";
 import RouteSelect from "./RouteSelect";
+import Plan from "./Plan";
 
 export default function Navbar(){
     return(
         <div>
-            <div className="overflow-y-scroll sticky top-4 h-[calc(100vh-32x-48px)] min-w-[220px]">
+            <div className=" sticky top-4 h-[calc(100vh-32x-48px)] min-w-[220px]">
                 <AccountToggle />
                 <SearchBar />
                 <RouteSelect />
+                
 
             </div>
-            
+            <Plan />
         </div>
     );
 
