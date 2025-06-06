7500285567dd689e84393fdbe8006c659030d5ea
pietroaa <167457324+pietroaandrade@users.noreply.github.com>
2025-06-05 22:09:05 -0300

    feat.esp32_code

--- arduino/main.ino
+++ arduino/main.ino
@@ -1,5 +1,8 @@
 #include <Wire.h>
 #include <LiquidCrystal_I2C.h>
+#include <WiFi.h>
+#include <PubSubClient.h>
+
 
 #define WATER_SENSOR_PIN 36  
 #define BUZZER_PIN 13
@@ -8,49 +11,114 @@
 #define YELLOW_LED 26
 #define RED_LED 27
 
-LiquidCrystal_I2C lcd(0x27, 16, 2);  
+LiquidCrystal_I2C lcd(0x27, 20, 4);  
 
 String sensorID = "Rua Moises";
+
+
+const char* ssid = "Wokwi-GUEST";
+const char* password = "";  
+const char* mqttServer = "test.mosquitto.org"; 
+const int mqttPort = 1883;
+const char* mqttTopic = "flood-monitor/data";
+
+WiFiClient espClient;
+PubSubClient client(espClient);
+
 
 void setup() {
   Serial.begin(115200);
 
+  
   pinMode(WATER_SENSOR_PIN, INPUT);
   pinMode(BUZZER_PIN, OUTPUT);
-
   pinMode(GREEN_LED, OUTPUT);
   pinMode(YELLOW_LED, OUTPUT);
   pinMode(RED_LED, OUTPUT);
 
+  
   lcd.init();
   lcd.backlight();
+
+  
+  WiFi.begin(ssid, password);
+  Serial.print("Conectando ao Wi-Fi");
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("\nWi-Fi conectado!");
+
+  
+  client.setServer(mqttServer, mqttPort);
+  while (!client.connected()) {
+    Serial.println("Conectando ao broker MQTT...");
+    if (client.connect("ESP32Client")) {
+      Serial.println("Conectado ao MQTT!");
+    } else {
+      Serial.print("Falha, rc=");
+      Serial.print(client.state());
+      delay(2000);
+    }
+  }
 }
+
 
 void loop() {
   int waterLevel = analogRead(WATER_SENSOR_PIN);
+
+  
+  float level_cm = map(waterLevel, 0, 4095, 0, 30); 
   int percent = map(waterLevel, 0, 4095, 0, 100);
 
+  lcd.clear();
+
+  
   lcd.setCursor(0, 0);
-  lcd.print("ID: ");
-  lcd.print(sensorID);
-  lcd.print("       ");
+  lcd.print("Flood Monitor");
 
+  
   lcd.setCursor(0, 1);
-  lcd.print("Level: ");
-  lcd.print(percent);
-  lcd.print("%    "); 
+  lcd.print("Location: ");
+  lcd.print(sensorID);
 
+  
+  lcd.setCursor(0, 2);
+  lcd.print("Level: ");
+  lcd.print(level_cm, 1);  
+  lcd.print(" cm        ");
+
+  
+  String risk = "";
   if (percent < 30) {
-    setRiskLevel("LOW");
+    risk = "LOW";
   } else if (percent < 70) {
-    setRiskLevel("MEDIUM");
+    risk = "MEDIUM";
   } else {
-    setRiskLevel("HIGH");
+    risk = "HIGH";
   }
 
-  delay(500);
-}
+  setRiskLevel(risk);
+  lcd.setCursor(0, 3);
+  lcd.print("Risk: " + risk);
 
+  delay(5000);
+
+  String json = "{\"location\":\"" + sensorID + "\",\"level_cm\":" + String(level_cm, 1) + ",\"risk\":\"" + risk + "\"}";
+  Serial.println(json);
+
+ 
+  if (client.connected()) {
+    client.publish(mqttTopic, json.c_str());
+  } else {
+    Serial.println("MQTT desconectado, tentando reconectar...");
+    client.connect("ESP32Client");
+  }
+
+  client.loop();
+  delay(5000);
+
+}
 
 void setRiskLevel(String level) {
   if (level == "LOW") {
